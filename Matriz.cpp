dddbfc1c46dc1f79015e8f906b25186f3ead7bd9
Samuel Silva <fcosamuel11@gmail.com>
2025-08-22 12:49:11 -0300

    adicionei a funcao para mostrar a matriz na tela

--- Matriz.cpp
+++ Matriz.cpp
@@ -4,6 +4,7 @@
 using namespace std;
     
     bool buscarmatriz(int numero[][4],int i,int j,int& x,int& linhas,int& colunas){
+
         if (i == linhas) return false;
 
         if (numero[i][j] == x) return true;
@@ -15,16 +16,44 @@ using namespace std;
             return  buscarmatriz(numero,i,j + 1,x,linhas,colunas);
         }
         
-
+    }
+     
+    void printfmatriz(int numero[][4],int i,int j,int& linhas,int& colunas){
         
+         if (i == linhas) return;
+
+         if (j == 0)
+         {
+            printf("{ ");
+         }
+         
+            int x = numero[i][j];
+            
+            printf("%d",x);
+            
+         if (j < colunas - 1)
+         {
+            printf(", ");
+         }
+         
+         if (j == colunas - 1)
+        {
+            printf("}\n");
+            return printfmatriz(numero,i + 1,0,linhas,colunas);
+        }else{
+            return  printfmatriz(numero,i,j + 1,linhas,colunas);
+        }
+         
+         
     }
 
 int main() {
-    int  numero[4][4] = {
+    int  numero[5][4] = {
         {8 , 9 , 10 , 20 },
         {30, 50 , 4 , 45 },
-        {2 , 1 , 22 , 3  },
+        {2 , 1 , 22 , 3},
         {11 , 200 , 5 , 7},
+        {12 , 13 , 14 , 15},
     };
 
     int linhas = sizeof(numero) / sizeof(numero[0]);       
@@ -42,6 +71,9 @@ int main() {
     }else{
         printf("Numero %d nao Pertence a matriz\n",x);
     }
+    
+    
+    printfmatriz(numero,0,0,linhas,colunas);
     return 0;
 
 }
